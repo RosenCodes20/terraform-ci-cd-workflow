22dc30dee188203be3637b015c13e21744f122ee
MacBookPro16 <rrirrirri08@gmail.com>
2025-08-06 14:14:36 +0300

    feat: added commands.sh

---
+++ commands.sh
@@ -0,0 +1,16 @@
+git init
+git add .
+git commit -m 'feat: repo init!'
+
+# Commit from VSCODE in the branch menu
+
+az ad sp create-for-rbac --name "Azure-Terraform-GitHub-Actions-Rosen" --role contributor --scopes /subscriptions/a2e8abe3-6046-4245-ba2d-32672ba88eda --sdk-auth
+
+# Create a service principal - then use the secrets in GitHub with the following names:
+# AZURE_CLIENT_ID
+# AZURE_CLIENT_SECRET
+# AZURE_TENANT_ID
+# AZURE_SUBSCRIPTION_ID
+# AZURE_CREDENTIALS
+
+# After that everything is done by itsself!!!
