ca0e398b7235b043f7f5cc380bc981c53744f938
MacBookPro16 <rrirrirri08@gmail.com>
2025-08-06 13:37:39 +0300

    feat: repo init!

---
+++ azure_resource_group.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.38.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = "a2e8abe3-6046-4245-ba2d-32672ba88eda"
+}
+
+resource "azurerm_resource_group" "terraformResourceGroupRosen" {
+  name     = "${var.resource_group_name}-${random_integer.ri.result}"
+  location = "${var.resource_group_location}"
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_service_plan" "aasp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.terraformResourceGroupRosen.name
+  location            = azurerm_resource_group.terraformResourceGroupRosen.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "alwa" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.terraformResourceGroupRosen.name
+  location            = azurerm_service_plan.aasp.location
+  service_plan_id     = azurerm_service_plan.aasp.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+
+    always_on = false
+  }
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.ams.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.amd.name};User ID=${azurerm_mssql_server.ams.administrator_login};Password=${azurerm_mssql_server.ams.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_mssql_server" "ams" {
+  name                         = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.terraformResourceGroupRosen.name
+  location                     = azurerm_resource_group.terraformResourceGroupRosen.location
+  version                      = "12.0"
+  administrator_login          = "${var.sql_server_admin_login}"
+  administrator_login_password = "${var.sql_server_admin_password}"
+}
+
+
+resource "azurerm_mssql_database" "amd" {
+  name         = "${var.sql_database_name}"
+  server_id    = azurerm_mssql_server.ams.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+}
+
+resource "azurerm_mssql_firewall_rule" "amfr" {
+  name             = "${var.firewall_rule_name}"
+  server_id        = azurerm_mssql_server.ams.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "aassc" {
+  app_id                 = azurerm_linux_web_app.alwa.id
+  repo_url               = "${var.repo_URL}"
+  branch                 = "main"
+  use_manual_integration = true
+}
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "resource_group_name" {
+    description = "The name of the resource group where the resources will be created."
+    type        = string
+}
+
+variable "resource_group_location" {
+    description = "The Azure region where the resource group will be created."
+    type        = string
+}
+
+variable "app_service_plan_name" {
+    description = "The name of the App Service Plan."
+    type        = string
+}
+
+variable "app_service_name" {
+  description = "value of the App Service name."
+    type        = string
+}
+
+variable "sql_server_name" {
+  description = "value of the SQL Server name."
+    type        = string
+}
+
+variable "sql_database_name" {
+  description = "The name of the SQL Database."
+  type        = string
+}
+
+variable "sql_server_admin_login" {
+  description = "The administrator login for the SQL Server."
+  type        = string
+}
+
+variable "sql_server_admin_password" {
+  description = "The administrator password for the SQL Server."
+  type        = string
+}
+
+variable "firewall_rule_name" {
+  description = "The name of the SQL Server firewall rule."
+  type        = string
+  
+}
+
+variable "repo_URL" {
+    description = "The URL of the repository to deploy to the App Service."
+    type        = string
+}
